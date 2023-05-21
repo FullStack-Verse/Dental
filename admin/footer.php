d9ef1d61b7553b326859321222092516c3c23389
MainakRepositor <sultankhilji001@gmail.com>
2023-05-21 18:03:39 +0530

    added files

---
+++ admin/footer.php
@@ -0,0 +1,11 @@
+<div class="container">   
+   <footer class="footer">
+      <div class="container">
+	  <div class="foot-margin">
+        <p><a>Copyright � 2023 Tooth Plus Clinic. All Rightsreserved at MWS</a></p>
+      </div>
+      </div>
+    </footer>
+</div>
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,17 @@
+    <footer class="footer">
+      <div class="container">
+	  <div class="foot-margin">
+          <p><a>Copyright � 2023 Tooth Plus Clinic. All Rightsreserved at MWS</a></p>
+      </div>
+      </div>
+    </footer>
+	
+		<script type="text/javascript">
+			$(function() {
+				$('#da-thumbs > li').hoverdir();
+			});
+		</script>
+<?php include('tooltip.php'); ?>
+
+</body>
+</html>
